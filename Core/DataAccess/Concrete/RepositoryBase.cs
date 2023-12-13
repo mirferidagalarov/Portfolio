ea3fbffb04e7b6bf5f1f7fbafc4b27010674634d
Mirferid Agalarov <mirferid.agalarov@gmail.com>
2023-12-13 17:17:03 +0400

    First

---
+++ Core/DataAccess/Concrete/RepositoryBase.cs
@@ -0,0 +1,61 @@
+﻿using Core.DataAccess.Abstract;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.Concrete
+{
+    public class RepositoryBase<TEntity, TContext> : IRepository<TEntity>
+        where TEntity : BaseEntity, new()
+        where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            using TContext context = new TContext();
+
+            var addedEntity = context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            context.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using TContext context = new TContext();
+
+            var deletedEntity = context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            context.SaveChanges();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            using TContext context = new TContext();
+            return context.Set<TEntity>().FirstOrDefault(predicate);
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate = null)
+        {
+            using TContext context = new();
+            return predicate is null
+                  ?
+                   context.Set<TEntity>().ToList()
+                  :
+                  context.Set<TEntity>().Where(predicate).ToList();
+
+        }
+
+        public void Update(TEntity entity)
+        {
+            using TContext context = new TContext();
+
+            var updatedEntity = context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DataAccess/Concrete/PortfolioDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete.TableModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class PortfolioDbContext:IdentityDbContext<User,Role,int> 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-HM4BSSR\\SQLEXPRESS;database=Porto;integrated security=true;");
+        }
+    }
+}
